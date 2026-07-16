c9524d5278c6a74455afee12110e3bc09b585821
ferdi-oz <ferdiozbunar@gmail.com>
2026-07-16 23:04:03 +0300

    v1.1 FamilyRepository completed

---
+++ src/types/FamilyMember.ts
@@ -0,0 +1,33 @@
+export interface FamilyMember {
+  id: string;
+
+  userId: string;
+
+  username: string;
+
+  email?: string;
+
+  age: number;
+
+  gender: string;
+
+  height: number;
+
+  weight: number;
+
+  allergies: string;
+
+  diseases: string;
+
+  diet: string;
+
+  activityLevel: string;
+
+  goal: string;
+
+  photo?: string;
+
+  createdAt: string;
+
+  updatedAt?: string;
+}
